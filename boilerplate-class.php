001e83af3613e15ebb7ef0bf5f4909b619e006e4
Fansocial <133858861+MadlinksCoding@users.noreply.github.com>
2024-12-23 11:34:35 +1000

    Add files via upload

---
+++ boilerplate-class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class:       {ClassName}
+ * Description: {Description / Purpose}
+ * Version:     1.0
+ * Since:       1.0
+ * Text Domain: fansocial
+ *
+ * @CodeQualityReview Version {version} // CTO to update.
+ * @CodingStandardsReview Version {version} // CTO to update.
+ * @CodeDocumented Version {version} {link} // CTO to update.
+ * @WPDetached {true/false} // CTO to update.
+ *
+ * @package MadLinksCoding
+ */
+
+// Define a namespace called MadCoding.
+namespace MadLinksCoding;
+
+// Prevent direct file access.
+if ( ! defined( 'APP_ENVIRONMENT' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Class: ClassName
+ */
+class ClassName {
+	
+}
